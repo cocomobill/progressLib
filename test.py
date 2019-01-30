02509bde810d4c192ada0213b881f90ed0bc60a3
Gokberk Yaltirakli <webdosusb@gmail.com>
2019-01-30 08:00:16 +0000

    test: Added percentage test cases

--- test.py
+++ test.py
@@ -4,68 +4,123 @@ import tempfile
 import subprocess
 import shutil
 
+
 class ProgressTest(unittest.TestCase):
     def test_width_0(self):
         for i in range(2):
-            result = run_cpp(f'Progress p(0, 100); std::cout << p.toBar({i});')
-            self.assertEqual(result, '')
+            self.assertCpp(f"Progress p(0, 100); std::cout << p.toBar({i});", "")
 
     def test_width_2_empty(self):
-        result = run_cpp('Progress p(0, 100); std::cout << p.toBar(2);')
-        self.assertEqual(result, '[]')
+        self.assertCpp("Progress p(0, 100); std::cout << p.toBar(2);", "[]")
 
     def test_width_2_full(self):
-        result = run_cpp('Progress p(0, 100); p.update(100); std::cout << p.toBar(2);')
-        self.assertEqual(result, '[]')
+        self.assertCpp(
+            "Progress p(0, 100); p.update(100); std::cout << p.toBar(2);", "[]"
+        )
 
     def test_width_3_empty(self):
-        result = run_cpp('Progress p(0, 100); std::cout << p.toBar(3);')
-        self.assertEqual(result, '[ ]')
+        self.assertCpp("Progress p(0, 100); std::cout << p.toBar(3);", "[ ]")
 
     def test_width_3_full(self):
-        result = run_cpp('Progress p(0, 100); p.update(100); std::cout << p.toBar(3);')
-        self.assertEqual(result, '[#]')
+        self.assertCpp(
+            "Progress p(0, 100); p.update(100); std::cout << p.toBar(3);", "[#]"
+        )
 
     def test_width_4_empty(self):
-        result = run_cpp('Progress p(0, 100); std::cout << p.toBar(4);')
-        self.assertEqual(result, '[  ]')
+        self.assertCpp("Progress p(0, 100); std::cout << p.toBar(4);", "[  ]")
 
     def test_width_4_half(self):
-        result = run_cpp('Progress p(0, 100); p.update(50); std::cout << p.toBar(4);')
-        self.assertEqual(result, '[# ]')
+        self.assertCpp(
+            "Progress p(0, 100); p.update(50); std::cout << p.toBar(4);", "[# ]"
+        )
 
     def test_width_4_full(self):
-        result = run_cpp('Progress p(0, 100); p.update(100); std::cout << p.toBar(4);')
-        self.assertEqual(result, '[##]')
+        self.assertCpp(
+            "Progress p(0, 100); p.update(100); std::cout << p.toBar(4);", "[##]"
+        )
+
+    def test_percentage_passthrough(self):
+        for i in [0, 25, 50, 100]:
+            self.assertCpp(
+                f"Progress p(0, 100); p.update({i}); std::cout << p.toPercentage(4);",
+                str(i),
+            )
+
+    def test_percentage_fractional(self):
+        for i in [0.25, 25.25, 50.25, 99.25]:
+            self.assertCpp(
+                f"Progress p(0, 100); p.update({i}); std::cout << p.toPercentage(4);",
+                str(i),
+            )
+
+    def test_padded_percentage(self):
+        self.assertCpp(
+            "Progress p(0, 100); p.update(10); std::cout << p.toPaddedPercentage(2);",
+            " 10.00",
+        )
+
+        self.assertCpp(
+            "Progress p(0, 1000); p.update(100); std::cout << p.toPaddedPercentage(2);",
+            " 10.00",
+        )
+
+        self.assertCpp(
+            "Progress p(0, 1000); p.update(505); std::cout << p.toPaddedPercentage(2);",
+            " 50.50",
+        )
+
+        self.assertCpp(
+            "Progress p(0, 1000); p.update(1000); std::cout << p.toPaddedPercentage(2);",
+            "100.00",
+        )
+
+    def assertCpp(self, code, expected):
+        result = run_cpp(code)
+        self.assertEqual(result, expected)
 
 
 # Ugliness
 
+
 def run_cpp(code: str) -> str:
     with tempfile.TemporaryDirectory() as tempdir:
-        shutil.copy('./Progress.h', tempdir)
-        shutil.copy('./Progress.cpp', tempdir)
-        with open(f'{tempdir}/Progress.h', 'a+') as progress:
-                progress.write('''
+        shutil.copy("./Progress.h", tempdir)
+        shutil.copy("./Progress.cpp", tempdir)
+        with open(f"{tempdir}/Progress.h", "a+") as progress:
+            progress.write(
+                """
 char *dtostrf (double val, signed char width, unsigned char prec, char *sout) {
   char fmt[20];
   sprintf(fmt, "%%%d.%df", width, prec);
   sprintf(sout, fmt, val);
   return sout;
 }
-''')
-        
-        with open(f'{tempdir}/main.cpp', 'w+') as code_file:
-            code_file.write('''
-#include <iostream>
-#include <cstdio>
-#include "Progress.cpp"
-int main() {
-CODE
-}'''.replace('CODE', code))
-        #print(open(f'{tempdir}/main.cpp').read())
-        subprocess.run(['c++', 'main.cpp', '-O0', '-o', 'main', '-fpermissive'], cwd=tempdir, capture_output=True)
-        return subprocess.run(['./main'], capture_output=True, cwd=tempdir).stdout.decode('utf-8')
+"""
+            )
 
-if __name__ == '__main__':
+        with open(f"{tempdir}/main.cpp", "w+") as code_file:
+            code_file.write(
+                """
+                #include <iostream>
+                #include <cstdio>
+                #include "Progress.cpp"
+                int main() {
+                CODE
+                }""".replace(
+                    "CODE", code
+                )
+            )
+        # print(open(f'{tempdir}/main.cpp').read())
+        subprocess.run(
+            ["c++", "main.cpp", "-O0", "-o", "main", "-fpermissive"],
+            cwd=tempdir,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        return subprocess.run(
+            ["./main"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=tempdir
+        ).stdout.decode("utf-8")
+
+
+if __name__ == "__main__":
     unittest.main()
