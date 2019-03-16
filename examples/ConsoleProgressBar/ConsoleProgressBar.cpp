364f45bc6814f5205bf3c9542ec3fb20d5e72240
root <root@vps374135.ovh.net>
2019-03-16 17:32:31 +0300

    Fixed fflush on console example.

--- examples/ConsoleProgressBar/ConsoleProgressBar.cpp
+++ examples/ConsoleProgressBar/ConsoleProgressBar.cpp
@@ -6,8 +6,7 @@ int main(){
     std::cout << "Hey! Now doing some stuff for next 120 seconds\n";
     for (int secondsSoFar=0; secondsSoFar<=120; secondsSoFar++){
         myProgress.update(secondsSoFar);
-        std::cout << "\r" << myProgress.toBar(80) << myProgress.toPaddedPercentage(2) << "% " << secondsSoFar << " seconds so far";
-        std::fflush (stdout);
+        std::cout << "\r" << myProgress.toBar(80) << myProgress.toPaddedPercentage(1) << "% " << secondsSoFar << " seconds so far" << std::flush;
         usleep(1000*1000);
     }
     std::cout << "\nHey Done!\n";
